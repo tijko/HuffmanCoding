dfc51d7fb68e8d601ed38d75f43c4a0453767114
tijko <oberon@oberon-Satellite-M645.(none)>
2012-11-29 20:57:04 -0500

    made a few funcs

--- HuffmanTree.py
+++ HuffmanTree.py
@@ -1,102 +1,71 @@
 from collections import Counter
 import sys
 
+def two_freq(tree, sec_queue, freq):
+    tree.append([freq[0][1] + freq[1][1], freq[0], freq[1]])
+    sec_queue.append([freq[0][1] + freq[1][1], [freq[0][1] + freq[1][1], freq[0], freq[1]]])    
+    freq = freq[2:]
+    return tree, sec_queue, freq
+
+def one_to_one(tree, sec_queue, freq):
+    for i in tree:
+        if i == sec_queue[0][1]:
+            hold = tree.index(i)
+            tree[tree.index(i)] = [sec_queue[0][0] + freq[0][1], sec_queue[0][1], freq[0]]
+            sec_queue.append([freq[0][1] + sec_queue[0][0], tree[hold]])
+            sec_queue = sec_queue[1:]
+            freq = freq[1:]
+            break
+    return tree, sec_queue, freq
+
+def two_sec(tree, sec_queue):
+    for i in tree:  
+        if i == sec_queue[0][1]:
+            hold = tree.index(i)
+            tree[tree.index(i)] = [sec_queue[0][0] + sec_queue[1][0], sec_queue[0][1], sec_queue[1][1]]
+            sec_queue.append([sec_queue[0][0] + sec_queue[1][0], tree[hold]])
+            for j in tree:
+                if j == sec_queue[1][1]:
+                    tree.remove(j)
+                    break
+            sec_queue = sec_queue[2:]
+            break
+    return tree, sec_queue
+
 def h_tree(data):
     tree = []
     sec_queue = []
     counts = Counter(data)
     freq = sorted([(i[0], i[1],) for i in zip(counts.keys(), counts.values())], key=lambda x: x[1])
-    sec_queue.append([freq[0][1] + freq[1][1], [freq[0][1] + freq[1][1], freq[0], freq[1]]])
-    tree.append([freq[0][1] + freq[1][1], freq[0], freq[1]])
-    freq = freq[2:]
+    tree, sec_queue, freq = two_freq(tree, sec_queue, freq)
     while len(freq) > 1:
         if len(sec_queue) < 2:
             if freq[0][1] + freq[1][1] <= freq[0][1] + sec_queue[0][0]:
-                tree.append([freq[0][1] + freq[1][1], freq[0], freq[1]])
-                sec_queue.append([freq[0][1] + freq[1][1], [freq[0][1] + freq[1][1], freq[0], freq[1]]])
-                freq = freq[2:]
+                tree, sec_queue, freq = two_freq(tree, sec_queue, freq)
             elif freq[0][1] + sec_queue[0][0] < freq[0][1] + freq[1][1]:
-                for i in tree:
-                    if i == sec_queue[0][1]:
-                        hold = tree.index(i)
-                        tree[tree.index(i)] = [sec_queue[0][0] + freq[0][1], sec_queue[0][1], freq[0]]
-                        sec_queue.append([freq[0][1] + sec_queue[0][0], tree[hold]])
-                        sec_queue = sec_queue[1:]
-                        freq = freq[1:]
-                        break
+                tree, sec_queue, freq = one_to_one(tree, sec_queue, freq)
             if len(freq) < 2:
                 break
         if len(sec_queue) > 1:
             if freq[0][1] + freq[1][1] <= freq[0][1] + sec_queue[0][0] and freq[0][1] + freq[1][1] <= sec_queue[0][0] + sec_queue[1][0]:
-                tree.append([freq[0][1] + freq[1][1], freq[0], freq[1]])
-                sec_queue.append([freq[0][1] + freq[1][1], [freq[0][1] + freq[1][1], freq[0], freq[1]]])
-                freq = freq[2:]
+                tree, sec_queue, freq = two_freq(tree, sec_queue, freq)
             elif freq[0][1] + sec_queue[0][0] < freq[0][1] + freq[1][1] and freq[0][1] + sec_queue[0][0] <= sec_queue[0][0] + sec_queue[1][0]:
-                for i in tree:
-                    if i == sec_queue[0][1]:
-                        hold = tree.index(i)
-                        tree[tree.index(i)] = [sec_queue[0][0] + freq[0][1], sec_queue[0][1], freq[0]]
-                        sec_queue.append([freq[0][1] + sec_queue[0][0], tree[hold]])
-                        sec_queue = sec_queue[1:]
-                        freq = freq[1:]
-                        break
+                tree, sec_queue, freq = one_to_one(tree, sec_queue, freq)
             elif sec_queue[0][0] + sec_queue[1][0] < freq[0][1] + freq[1][1] and \
                 sec_queue[0][0] + sec_queue[1][0] < sec_queue[0][0] + freq[0][1]:
-                for i in tree:
-                    if i == sec_queue[0][1]:
-                        hold = tree.index(i)
-                        tree[tree.index(i)] = [sec_queue[0][0] + sec_queue[1][0], sec_queue[0][1], sec_queue[1][1]]
-                        sec_queue.append([sec_queue[0][0] + sec_queue[1][0], tree[hold]])
-                        for j in tree:
-                            if j == sec_queue[1][1]:
-                                tree.remove(j)
-                                break
-                        sec_queue = sec_queue[2:]
-                        break
+                tree, sec_queue = two_sec(tree, sec_queue)
     while len(sec_queue) > 1:
         if freq:
             if freq[0][1] + sec_queue[0][0] < sec_queue[0][0] + sec_queue[1][0]:
-                for i in tree:
-                    if i == sec_queue[0][1]:
-                        hold = tree.index(i)
-                        tree[tree.index(i)] = [sec_queue[0][0] + freq[0][1], sec_queue[0][1], freq[0]]
-                        sec_queue.append([freq[0][1] + sec_queue[0][0], tree[hold]])
-                        sec_queue = sec_queue[1:]
-                        freq = freq[1:]
-                        break
+                tree, sec_queue, freq = one_to_one(tree, sec_queue, freq)
             else:
-                for i in tree:
-                    if i == sec_queue[0][1]:
-                        hold = tree.index(i)
-                        tree[tree.index(i)] = [sec_queue[0][0] + sec_queue[1][0], sec_queue[0][1], sec_queue[1][1]]
-                        sec_queue.append([sec_queue[0][0] + sec_queue[1][0], tree[hold]])
-                        for j in tree:
-                            if j == sec_queue[1][1]:
-                                tree.remove(j)
-                                break 
-                        sec_queue = sec_queue[2:]
-                        break
+                tree, sec_queue = two_sec(tree, sec_queue)
         else:
-            for i in tree:
-                if i == sec_queue[0][1]:
-                    hold = tree.index(i)
-                    tree[tree.index(i)] = [sec_queue[0][0] + sec_queue[1][0], sec_queue[0][1], sec_queue[1][1]]
-                    sec_queue.append([sec_queue[0][0] + sec_queue[1][0], tree[hold]])
-                    for j in tree:
-                        if j == sec_queue[1][1]:
-                            tree.remove(j)
-                            break
-                    sec_queue = sec_queue[2:]
-                    break
+            tree, sec_queue = two_sec(tree, sec_queue)
     if freq:
-        for i in tree:
-            if i == sec_queue[0][1]:
-                hold = tree.index(i)
-                tree[tree.index(i)] = [sec_queue[0][0] + freq[0][1], sec_queue[0][1], freq[0]]
-                sec_queue.append([freq[0][1] + sec_queue[0][0], tree[hold]])
-                sec_queue = sec_queue[1:]
-                freq = freq[1:]
-                break
+        tree, sec_queue, freq = one_to_one(tree, sec_queue, freq)
     return tree[0]
 
 print h_tree(sys.argv[1])
+
+
