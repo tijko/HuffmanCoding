ba613d326c7082c2ddc198717cf3f26966a6dae3
tijko <oberon@oberon-Satellite-M645.(none)>
2012-12-03 15:17:49 -0500

    using the heap module

---
+++ heap.py
@@ -0,0 +1,55 @@
+from heapq import heappop, heappush, heapify
+from collections import Counter
+import sys
+
+class HuffmanCoding(object):
+
+    def __init__(self):
+        self.tree = [] 
+
+    def huff_bin(self, tree, encode, data=None):
+        if data == None:
+            data = {}
+        for i in tree:
+            if isinstance(i, list) or isinstance(i, tuple):
+                if tree.index(i) == 1:
+                    old_encode = encode
+                    encode += '0'
+                    self.huff_bin(i, encode, data)
+                if tree.index(i) == 2:
+                    encode = old_encode
+                    encode += '1'
+                    self.huff_bin(i, encode, data)
+            if isinstance(i, str):
+                data[i] = encode
+        return data
+
+    def construct_tree(self, data):
+        char_freq = Counter(data)
+        queue = [(char_freq[i],i,) for i in char_freq]
+        heapify(queue)
+        while len(queue) > 1:
+            q1 = heappop(queue)
+            q2 = heappop(queue)
+            branch = (q1[0] + q2[0], q1[1] + q2[1],)
+            heappush(queue, branch)
+            if len(q1[1]) < 2:
+                self.tree.append([q1[0] + q2[0],q1, q2])
+            elif len(q1[1]) > 1 and len(q2[1]) > 1:
+                self.tree.append([self.tree[0][0] + self.tree[1][0], self.tree[0], self.tree[1]])
+                self.tree.pop(0)
+                self.tree.pop(0) 
+        return self.tree[0]
+
+
+hc = HuffmanCoding()
+t = hc.construct_tree(sys.argv[1])
+compress = hc.huff_bin(t, '')
+encoding = ''
+stream = []
+for i in  sys.argv[1]:
+    if compress.has_key(i):
+        stream.append(compress[i])
+        encoding += compress[i]
+
+print encoding
